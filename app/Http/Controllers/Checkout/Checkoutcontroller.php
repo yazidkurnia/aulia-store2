1f4cc822cbafe1ecefddfe5cc68e2a195445d228
yazidkurnia <zidyazid222@gmail.com>
2023-07-26 18:19:10 +0700

    update:lakukan pengurangan stock saat checkout

--- app/Http/Controllers/Checkout/Checkoutcontroller.php
+++ app/Http/Controllers/Checkout/Checkoutcontroller.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Checkout;
 use App\Models\Cart;
+use App\Models\Stock;
 use DB;
 
 class Checkoutcontroller extends Controller
@@ -34,13 +35,22 @@ class Checkoutcontroller extends Controller
             $itemCheckout = Cart::where(['status' => 1, 'order_status' => 1])->get();
 
             // dd($itemCheckout);
-            DB::beginTransaction(function() {
-                $itemCheckout->order_code = $transactionCode;
-                $itemCheckout->save();
-                // NOTE:: get stock
-                $getStock = Stock::where('item_id', $itemCheckout->id)->first();
-            });
+           foreach ($itemCheckout as $key => $value) {
+                DB::beginTransaction();
+                // NOTE:: update order code of cart
+                $value->order_code = $transactionCode;
+                $value->save();
+       
+                // NOTE:: get summary of total stock by item id
+                $totalStockByProductId = Stock::where('item_id', $value->product_id)->sum('total_stock');
+                // NOTE:: do operation to stock
+                Stock::where('item_id', $value->product_id)
+                     ->update(['total_stock' => $totalStockByProductId - $value->totalqty]);
+                
+                DB::commit();
+            }
 
+            
             return response()->json([
                 'status_code' => 200,
                 'status_maessage' => ['Success', 'Success checkout'],
